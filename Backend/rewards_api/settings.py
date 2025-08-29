f10218375ffa37c8728fe83ab9b9f63f4e1f7fea
keneilwe23 <keneilwem333@gmail.com>
2025-08-29 09:11:23 +0200

    Update settings.py

---
+++ Backend/rewards_api/settings.py
@@ -0,0 +1,14 @@
+DEBUG = True
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    'rewards_api',
+]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
